84e7b87f17ad04ba3353e9db6888c4ef8bea6c1c
Collins shibi kurian <146367412+xperia3110@users.noreply.github.com>
2024-11-16 23:43:15 +0530

    Update waypoint_service.py

--- waypoint_service.py
+++ waypoint_service.py
@@ -1,21 +1,46 @@
+#!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Pose
 from waypoint_navigation.srv import GetWaypoints
 
-class WaypointService(Node):
+class WayPoints(Node):
+
     def __init__(self):
-        super().__init__('waypoint_service')
-        self.srv = self.create_service(GetWaypoints, 'get_waypoints', self.get_waypoints_callback)
-        self.get_logger().info("Waypoint Service started")
+        super().__init__('waypoints_service')
+        self.srv = self.create_service(GetWaypoints, 'waypoints', self.waypoint_callback)
+        self.waypoints = [[2.0, 2.0, 27.0], [2.0, -2.0, 27.0], [-2.0, -2.0, 27.0], [-2.0, 2.0, 27.0], [1.0, 1.0, 27.0]]
 
-    def get_waypoints_callback(self, request, response):
-        response.waypoints_x = [1.0, 2.0, 3.0, 4.0, 5.0]
-        response.waypoints_y = [1.0, 2.0, 3.0, 4.0, 5.0]
-        response.waypoints_z = [2.0, 2.0, 2.0, 2.0, 2.0]
-        return response
+    
+    def waypoint_callback(self, request, response):
 
-def main(args=None):
-    rclpy.init(args=args)
-    waypoint_service = WaypointService()
-    rclpy.spin(waypoint_service)
-    rclpy.shutdown()
+        if request.get_waypoints == True :
+            response.waypoints.poses = [Pose() for _ in range(len(self.waypoints))]
+            for i in range(len(self.waypoints)):
+                response.waypoints.poses[i].position.x = self.waypoints[i][0]
+                response.waypoints.poses[i].position.y = self.waypoints[i][1]
+                response.waypoints.poses[i].position.z = self.waypoints[i][2]
+            self.get_logger().info("Incoming request for Waypoints")
+            return response
+
+        else:
+            self.get_logger().info("Request rejected")
+
+def main():
+    rclpy.init()
+    waypoints = WayPoints()
+
+    try:
+        rclpy.spin(waypoints)
+    except KeyboardInterrupt:
+        waypoints.get_logger().info('KeyboardInterrupt, shutting down.\n')
+    finally:
+        waypoints.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+        
+
+        
